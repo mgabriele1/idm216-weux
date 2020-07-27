b774def922e1069f5aee75dc7fd06dff76dd2b7c
Melissa Gabriele <meg394@drexel.edu>
2020-07-27 14:06:36 -0400

    Ignored db
    passwords ignored

--- backend/includes/_db.php
+++ backend/includes/_db.php
@@ -2,18 +2,18 @@
 
 <?php
 //local
-//  $dbhost = "localhost";
-//  $dbuser = "root";
-//  $dbpass = "root";
-//  $dbname = "imdb";
-//  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "root";
+ $dbname = "imdb";
+ $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // online
-$dbhost = "localhost";
-$dbuser = "mgabrie1_idm216";
-$dbpass = "idm216";
-$dbname = "mgabrie1_imdb";
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+// $dbhost = "localhost";
+// $dbuser = "mgabrie1_idm216";
+// $dbpass = "idm216";
+// $dbname = "mgabrie1_imdb";
+// $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
   // Check the connection is good with no errors
   if (mysqli_connect_errno()) {
